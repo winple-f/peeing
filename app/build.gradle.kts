d645d14e2746b6dc40b3186ef602aabe6aff4569
winplefsy <winplefsy@outlook.com>
2026-09-19 20:01:52 +0800

    所有版本号加上beta前缀

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "beta-1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
